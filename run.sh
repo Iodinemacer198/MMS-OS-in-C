bdb507b5ae107538d810a05dda3e7d2c44c71d57
Iodinemacer198 <tylermolina508@gmail.com>
2026-03-10 17:04:20 -0400

    Changed run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-qemu-system-x86_64 -cdrom iso/output/mms-os.iso -drive format=raw,file=disk.img
+FILE="disk.img"
+
+if [ -f "$FILE" ]; then
+    qemu-system-x86_64 -cdrom iso/output/mms-os.iso -drive format=raw,file=disk.img
+else
+    qemu-img create -f raw disk.img 10M
+    echo "Created filesystem image"
+    qemu-system-x86_64 -cdrom iso/output/mms-os.iso -drive format=raw,file=disk.img
+fi
